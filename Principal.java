cdf41489643bfb8e4b39638348e3ca12b1e1cfb0
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-05-16 15:17:28 +0000

    abrstracao

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        
+        Circulo circulo = new Circulo();
+
+        circulo.setRaio(10);
+        System.out.println(circulo.calcularArea());
+        
+    }
+    
+}
